cfa084f02ff986f1333149513928448b640aa4d3
aftab0045 <aftabattar0045@gmail.com>
2026-03-18 11:30:01 +0530

    add

---
+++ terraform-practice/modules/EC2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-02dfbd4ff395f2a1b"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "N Virginia Key"
+}
+
+variable "security_group_id" {
+  default = "sg-0398e23bfa06598ce"
+}
