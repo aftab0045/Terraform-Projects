891a7b785abc57f67fef68eb1392242f93d09e9e
aftab0045 <aftabattar0045@gmail.com>
2026-03-18 12:25:50 +0530

    add

--- terraform-practice/modules/EC2/main.tf
+++ terraform-practice/modules/EC2/main.tf
@@ -4,9 +4,26 @@ resource "aws_instance" "module_instance" {
   key_name = var.key_name
   vpc_security_group_ids = [var.security_group_id]
 
-  # Defining local provisioner 
-    provisioner "local-exec" {
+  # Defining local provisioner block
+    /*provisioner "local-exec" {
         command = "echo ${aws_instance.module_instance.public_ip} > public_ip.txt"
+    }*/
+
+  # Defining remote provisioner block
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum update -y",
+            "sudo yum install nginx -y",
+            "sudo systemctl start nginx",
+            "sudo systemctl enable nginx",
+        ]
+
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = file("C:/Downloads/N Virginia Key.pem")
+            host = self.public_ip
+        }
     }
 
   tags = {
