ef2c4f6843c1174982fd90065913d3171ddd66e0
aftab0045 <aftabattar0045@gmail.com>
2026-03-18 11:35:01 +0530

    add

--- terraform-practice/modules/EC2/main.tf
+++ terraform-practice/modules/EC2/main.tf
@@ -3,4 +3,8 @@ resource "aws_instance" "module_instance" {
   instance_type = var.instance_type
   key_name = var.key_name
   vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = "Terraform-Module-Instance"
+  }
 }
