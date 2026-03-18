ffec238d54dd8d61a303f397f0e9ef5673c5cda6
aftab0045 <aftabattar0045@gmail.com>
2026-03-18 12:19:04 +0530

    add

--- terraform-practice/modules/EC2/main.tf
+++ terraform-practice/modules/EC2/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "module_instance" {
   key_name = var.key_name
   vpc_security_group_ids = [var.security_group_id]
 
+  # Defining local provisioner 
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.module_instance.public_ip} > public_ip.txt"
+    }
+
   tags = {
     Name = "Terraform-Module-Instance"
   }
